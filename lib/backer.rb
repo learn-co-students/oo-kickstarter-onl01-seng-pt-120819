4c48933449b17d19d1badc01b5582b83931050cc
Olesya Miller <olesyam.miller@gmail.com>
2019-12-25 15:11:23 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Backer 
+  attr_reader :name, :backed_projects
+  attr_writer :name 
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project 
+    project.backers <<  self 
+  end
+  
+  
+  
+  
+  
+end 
