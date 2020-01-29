f9a88714df0e929658bedb71352ac8d4b3f07579
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-29 02:42:10 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+
+  def back_project(title)
+
+
+  end
+
+
+
+
+end
