c467c6c458ed1bb4ebfcd92e115f698d2d093d63
William Pannell <lpann5@gmail.com>
2020-04-01 15:29:57 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,4 +4,12 @@ class Backer
     @name = name
     @backed_projects = []
   end
+  
+  def back_project(project)
+    @backed_projects << project
+    if !project.backers.include?(self)
+      project.add_backer(self)
+    end
+  end
+
 end
